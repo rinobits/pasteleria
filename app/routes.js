8dbe7878605d7177112abeb46350dab6f0cd1a32
macondo <jl.durand95@gmail.com>
2020-06-19 14:21:09 -0400

    gitUpdate

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,12 @@
 const express    = require('express');
 
 // imports & consts 
-const users      = require('./users/routes.js');
-
+const users      = require('./users/routes');
+const auth       = require('./auth/routes')
 const index = (app) => {
     const router = express.Router();
     app.use('/', router);
+    router.use('/auth', auth);
     router.use('/users', users);
 }
 
